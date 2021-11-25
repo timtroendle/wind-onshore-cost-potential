ce5e117e08b0e408764783dc9f0a85563bb88a54
raffaele-sg <57661452+raffaele-sg@users.noreply.github.com>
2021-11-25 19:36:30 +0100

    Doas data from .tif and draft function to produce population by radius

--- scripts/population.py
+++ scripts/population.py
@@ -1,8 +1,13 @@
 import os
 import requests
-from zipfile import ZipFile
+import rasterio
+
+import numpy as np
+import pandas as pd
 
 from settings import DIR_DATA
+from simpledbf import Dbf5
+from zipfile import ZipFile
 
 
 def download_url(url, save_path, chunk_size=128) -> str:
@@ -25,27 +30,76 @@ def download_url(url, save_path, chunk_size=128) -> str:
         raise Exception(f'Error: request status code is not ok (200): {r.status_code}')
 
 
-def unzip_file(file_name, archive, destination) -> str:
-    if os.path.join(destination, file_name):
-        return os.path.join(destination, file_name)
+def unzip_file(file_name, archive, destination_folder) -> str:
+    file_full_path = os.path.join(destination_folder, file_name)
+
+    if os.path.isfile(file_full_path):
+        return file_full_path
 
     with ZipFile(archive, 'r') as zipObj:
         listOfiles = zipObj.namelist()
         if file_name not in listOfiles:
             raise Exception(f'Error: the file named {file_name} is not contained in the archive {archive}. Use one of the following file names:', listOfiles)
         else:
-            zipObj.extract(file_name, destination)
-            return os.path.join(destination, file_name)
+            zipObj.extract(file_name, destination_folder)
+            return file_full_path
 
 
-def get_file(file_name) -> str:
+def get_JRC_GRID_2018(file_name) -> str:
     archive = download_url(
         url=r'https://ec.europa.eu/eurostat/cache/GISCO/geodatafiles/JRC_GRID_2018.zip',
         save_path=os.path.join(DIR_DATA, r'JRC_GRID_2018.zip')
     )
 
-    return unzip_file(file_name, archive=archive, destination=DIR_DATA)
+    return unzip_file(file_name, archive=archive, destination_folder=DIR_DATA)
+
+
+
+def population_in_radius(radius=None) -> str:
+
+    assert isinstance(radius, int) or radius is None, 'Radius can also be an int or None because of how the file names are generated'
+
+    if radius:
+        return NotImplementedError
+
+    file_name = f'poulation_{radius}r.tif' if radius else 'poulation.tif'
+    
+    file_full_path = os.path.join(DIR_DATA, file_name)
+    if os.path.isfile(file_full_path):
+        return file_full_path
+
+    source_tif = get_JRC_GRID_2018('JRC_1K_POP_2018.tif')
+    dataset = rasterio.open(source_tif)
+    
+    index, = dataset.indexes
+    
+    data = dataset.read(index)
+    data = np.where(data==dataset.nodata, np.nan, data)
+
+    # https://rasterio.readthedocs.io/en/latest/quickstart.html#opening-a-dataset-in-writing-mode
+    with rasterio.open(
+        file_full_path,
+        'w',
+        driver='GTiff',
+        height=dataset.height,
+        width=dataset.width,
+        count=1,
+        dtype=data.dtype,
+        crs='+proj=latlong',    # TODO: QA, maybe can be taken from the original .tif property
+        compress='lzw',         # TODO: Are we sure that the original .tif was losslessly comporessed, and here also?
+        transform=dataset.transform,
+    ) as dst:
+        dst.nodata = 0.0
+        dst.write(data, 1)
+    
+    return file_full_path
 
 
 if __name__ == '__main__':
-    get_file('JRC_POPULATION_2018.dbf')
+    from matplotlib import pyplot
+     
+    population_tif = population_in_radius()
+    
+    # src = rasterio.open(population_tif)
+    # pyplot.imshow(src.read(1), cmap=pyplot.get_cmap('gray_r'), vmin=0, vmax=1000)
+    # pyplot.show()
