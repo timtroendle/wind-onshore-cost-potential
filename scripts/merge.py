9dd7489d0395a3b3fe3c256c96ba2b032c2aa9d9
Tim Tröndle <tim.troendle@usys.ethz.ch>
2022-01-21 14:20:44 +0100

    Update cost lookup to increase performance
    Co-authored-by: raffaele-sg <57661452+raffaele-sg@users.noreply.github.com>
    
    Fix #5.

--- scripts/merge.py
+++ scripts/merge.py
@@ -6,13 +6,16 @@ import rioxarray
 
 
 def merge(path_to_turbine_locations: str, path_to_lcoe: str, path_to_disamenity_cost: str, path_to_output: str):
-    turbines = pd.read_csv(path_to_turbine_locations, index_col=0)[:100] # FIXME see https://github.com/timtroendle/wind-externalities/issues/5
+    turbines = pd.read_csv(path_to_turbine_locations, index_col=0)
     lcoe = rioxarray.open_rasterio(path_to_lcoe)
     disamenity_cost = rioxarray.open_rasterio(path_to_disamenity_cost)
     assert_same_coords(lcoe, disamenity_cost)
 
+    x_res = int(lcoe.x.diff("x")[0].item()) // 2
+    y_res = int(lcoe.y.diff("y")[0].item()) // 2
+
     map_coords = turbines.apply(
-        lambda row: infer_map_coords(lcoe, x=row.x_m, y=row.y_m),
+        lambda row: infer_map_coords(lcoe, x=row.x_m, y=row.y_m, x_res=x_res, y_res=y_res),
         result_type="expand",
         axis=1
     )
@@ -25,15 +28,27 @@ def merge(path_to_turbine_locations: str, path_to_lcoe: str, path_to_disamenity_
     )
 
 
-def infer_map_coords(map: xr.DataArray, x: int, y: int): # TODO move to Python 3.9 to improve type hints
+def infer_map_coords(map: xr.DataArray, x: int, y: int, x_res: int, y_res: int): # TODO move to Python 3.9 to improve type hints
     """Returns map coordinates closest to input coordinates."""
-    map_x = abs(map.x - x).idxmin().item()
-    map_y = abs(map.y - y).idxmin().item()
+    x_proximity = map.x.loc[slice(x - x_res, x + x_res)]
+    y_proximity = map.y.loc[slice(y - y_res, y + y_res)]
+    try:
+        map_x = x_proximity.item()
+    except ValueError:
+        # handle case where x is equally far from two map points
+        assert x_proximity.count() == 2, f"Exactly 2 values expected in proximity of {x}. Check resolution: {x_proximity}"
+        map_x = x_proximity.values[0] # always take the smaller of the two
+    try:
+        map_y = y_proximity.item()
+    except ValueError:
+        # handle case where y is equally far from two map points
+        assert y_proximity.count() == 2, f"Exactly 2 values expected in proximity of {y}. Check resolution: {y_proximity}"
+        map_y = y_proximity.values[0] # always take the smaller of the two
     return {"x": map_x, "y": map_y}
 
 
 def assert_same_coords(da1: xr.DataArray, da2: xr.DataArray):
-    # assert da1.spatial_ref.crs_wkt == da2.spatial_ref.crs_wkt # FIXME fails due to https://github.com/timtroendle/wind-externalities/issues/7
+    assert da1.spatial_ref.crs_wkt == da2.spatial_ref.crs_wkt
     assert len(da1.x) == len(da2.x)
     assert len(da2.y) == len(da2.y)
     assert da1.spatial_ref.GeoTransform == da2.spatial_ref.GeoTransform
